5882a3cb730d96923a5bfbd11480633abc29b922
Jay Strybis <jay.strybis@gmail.com>
2014-01-20 12:26:23 -0600

    line things up

--- lib/tp_plus/nodes/io_node.rb
+++ lib/tp_plus/nodes/io_node.rb
@@ -3,8 +3,8 @@ module TPPlus
     class IONode
       attr_accessor :comment
       def initialize(type, id)
-        @type = type
-        @id = id.to_i
+        @type    = type
+        @id      = id.to_i
         @comment = ""
       end
 
