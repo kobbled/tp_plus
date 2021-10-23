d4c84e9965d0cbf3e629950527227590ac19caed
kobbled <mattpdewar@gmail.com>
2021-10-23 00:57:26 -0600

    use base object in inheritance tree

--- lib/tp_plus/nodes/definition_node.rb
+++ lib/tp_plus/nodes/definition_node.rb
@@ -8,7 +8,7 @@ module TPPlus
       end
 
       def eval(context)
-        if @assignable.is_a?(DigitNode) || @assignable.is_a?(RealNode) || @assignable.is_a?(StringNode)
+        if @assignable.is_a?(ConstNode) || @assignable.is_a?(StringNode)
           raise "Constants must be defined with all CAPS" unless @identifier.upcase == @identifier
 
           context.add_constant(@identifier, @assignable)
