ca787d36c7f0fa112edf14df6a5f1a233e681872
Matt Dewar <mattpdewar@gmail.com>
2021-10-25 04:00:37 -0600

    In use_uframe/utool update @pose_list frames. Handle different syntax types.

--- lib/tp_plus/nodes/use_node.rb
+++ lib/tp_plus/nodes/use_node.rb
@@ -10,6 +10,30 @@ module TPPlus
         end
       end
 
+      def update_pos_list(context)
+        if @value.is_a?(Nodes::VarNode) && !(@value.constant?)
+          if @value.constant?
+            if @type == "use_uframe"
+              context.pose_list.set_frame(context.get_const(@value.identifier).id)
+            else
+              context.pose_list.set_tool(context.get_const(@value.identifier).id)
+            end
+          else
+            if @type == "use_uframe"
+              context.pose_list.set_frame(context.get_var(@value.identifier).id)
+            else
+              context.pose_list.set_tool(context.get_var(@value.identifier).id)
+            end
+          end
+        elsif  @value.is_a?(Nodes::DigitNode)
+          if @type == "use_uframe"
+            context.pose_list.set_frame(@value.value)
+          else
+            context.pose_list.set_tool(@value.value)
+          end
+        end
+      end
+
       def eval(context)
         s = @value.eval(context)
         if @value.is_a?(Nodes::VarNode) && !(@value.constant?)
@@ -23,8 +47,10 @@ module TPPlus
 
         case @type
         when "use_uframe"
+          update_pos_list(context)
           "UFRAME_NUM=#{s}"
         when "use_utool"
+          update_pos_list(context)
           "UTOOL_NUM=#{s}"
         when "use_payload"
           "PAYLOAD[#{s}]"
