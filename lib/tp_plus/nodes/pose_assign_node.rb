d6d8e3ad28fded0e627ad06e5755e7b1f6dc2be2
kobbled <mattpdewar@gmail.com>
2021-10-30 00:01:32 -0600

    Refactor PoseAssignNode to use RangeNodes

--- lib/tp_plus/nodes/pose_assign_node.rb
+++ lib/tp_plus/nodes/pose_assign_node.rb
@@ -8,28 +8,10 @@ module TPPlus
         @options = options
       end
 
-      def getBaseName(name, context)
-        #remove numebrs from end of string
-        name.target_node(context).comment.gsub(/ *\d+$/, '')
-      end
-
-      def createNameArray(startId, endId, context)
-        raise "Start and end identifers for an array must have the same prefix" unless getBaseName(startId, context) == getBaseName(endId, context)
-        #turn an array of numbers into an array of strings with a prefix in front of the number
-        arr = *(startId.target_node(context).id..endId.target_node(context).id)
-        
-        prefix = getBaseName(startId, context)
-        arr.each_with_index do |p, index|
-          arr[index] = prefix + p.to_s
-        end
-
-        arr
-      end
-
       def eval(context)
         
-        array1 = createNameArray(@range1[:start], @range1[:end], context)
-        array2 = createNameArray(@range2[:start], @range2[:end], context)
+        array1 = @range1.name_range
+        array2 = @range2.name_range
 
         if @options
           if @options[:mod] == 'reverse'
