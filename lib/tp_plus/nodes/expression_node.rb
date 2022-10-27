3090e85a57b6d2ca0c22a3eb7bb6d62a2c8dbfb0
kobbled <mattpdewar@gmail.com>
2022-10-26 18:10:54 -0600

    add methods in ExpressionNode to store any function calls in an expression, as well as creating dummy return variables on the local stack
    `$dvar_counter` is used as a counter for the dummy variables to give them a unique name. this variable was made global for ease of use.

--- lib/tp_plus/nodes/expression_node.rb
+++ lib/tp_plus/nodes/expression_node.rb
@@ -6,6 +6,11 @@ module TPPlus
         @left_op  = left_op
         @op       = OperatorNode.new(op_string)
         @right_op = right_op
+
+        @func_exp = []
+        contains_call?(@func_exp)
+        @ret_var = []
+        create_ret_var(@func_exp, @ret_var)
       end
 
       def grouped?
@@ -28,12 +33,28 @@ module TPPlus
           [@left_op, @right_op].map { |op| op.is_a? ParenExpressionNode }.any?
       end
 
+      def contains_call?(flist)
+        flist << @left_op if @left_op.is_a? CallNode
+        flist << @right_op if @right_op.is_a? CallNode
+      end
+
       def boolean_result?
         case @op.string
         when "&&","||","!","==","<>",">",">=","<","<="
           true
         else
           false
+        end
+      end
+
+      def create_ret_var(flist, rlist)
+        flist.each do |fl|
+          #create local variable
+          $dvar_counter += 1
+          name = "dvar#{$dvar_counter}"
+          fl.set_return(VarNode.new(name))
+
+          rlist << RegDefinitionNode.new(name, LocalDefinitionNode.new('LR', name))
         end
       end
 
